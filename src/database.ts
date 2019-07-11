4d1d889518f5ac01a9ed5f4490839d41914c1b4d
Blue Sapphire <49204836+bluesapp@users.noreply.github.com>
2019-07-11 08:24:46 -0500

    Update database.ts

--- src/database.ts
+++ src/database.ts
@@ -3,9 +3,9 @@ const oracledb = require('oracledb');
 
 
 const config = {
-  user: 'basdat',
-  password: 'basdat',
-  connectString: '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.61.8)(PORT=1581))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=orcl.carvajal.com)))'
+  user: '',
+  password: '',
+  connectString: '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=192.168)(PORT=1581))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=orcl.c***.com)))'
 
 }
 
